8e81ecc17ab8d8b713c039b218792d52474be7bc
inovue <inovue3@gmail.com>
2023-06-09 14:56:59 +0900

    first commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# postCreateCommand.sh
+
+echo "START Install"
+
+sudo chown -R vscode:vscode .
+
+
+echo "First of all, please execute the following command"
+echo "-----------------------------------------------"
+echo "git config --global user.name \"Your Name\""
+echo "git config --global user.email \"Your Email\""
+echo "-----------------------------------------------"
+
+echo "FINISH Install"
---
+++ README.md
@@ -0,0 +1,12 @@
+### References
+* [devcontainers/images](https://github.com/devcontainers/images/tree/main/src/typescript-node)
+* [Available Dev Container Features](https://containers.dev/features)
+
+
+
+### Create a new project
+```
+npx create-remix@latest
+npx prisma init
+npx supabase init
+```
