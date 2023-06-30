a8b396804d25eecaeb6419469938f16750b87012
inovue <inouve3@gmail.com>
2023-06-30 18:49:21 +0900

    image delete action

--- remix/app/routes/stocks_.$id.edit.images.$mediaId.tsx
+++ remix/app/routes/stocks_.$id.edit.images.$mediaId.tsx
@@ -1,9 +1,9 @@
 import type { DataFunctionArgs } from "@remix-run/node";
 
 import { createServerClient } from "~/utils/supabase.server";
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { db } from "~/services/db.server";
-import { useLoaderData, useRouteLoaderData, useSubmit } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import { classNames } from "primereact/utils";
 
 export const loader = async ({ request, context, params }:DataFunctionArgs) => {
@@ -44,16 +44,20 @@ export const action = async ({ request, context, params }:DataFunctionArgs) => {
       const supabaseClient = createServerClient({request, response});
       const bucketName = process.env.SUPABASE_STORAGE_BUCKET ?? 'images';
 
-      const {data:removeResult, error} = await supabaseClient.storage.from(bucketName).remove([media.path]);
+      const {error} = await supabaseClient.storage.from(bucketName).remove([media.path]);
       if(error) throw new Response(error.message, { status: 500 });
 
-      return json({ files: removeResult });
+      await db.media.delete({where:{id:+mediaId}});
+      
+      return redirect('../')
     }catch(e){
       if(e instanceof TypeError) throw new Response(e.message, { status: 400 });
       else if(e instanceof Error) throw new Response(e.message, { status: 500 });
       else throw new Response("Unknown error", { status: 500 });
     }
   
+  }else{
+    throw new Response("Method not allowed", { status: 405 });
   }
   
 };
@@ -66,6 +70,9 @@ export default function StockEditImage() {
   return (
     <div style={{height:'60vh', backgroundColor:'black'}} className={classNames(['w-full', 'relative', 'py-3'])}>
       <img src={media.publicUrl} alt={media.filename} style={{objectFit:"contain"}} className={classNames(['w-full', 'h-full'])} />
+      <Form method="delete" className={classNames(['absolute', 'top-0', 'right-0', 'p-3'])}>
+        <button type="submit" className={classNames(['p-button', 'p-button-danger'])}>Delete</button>
+      </Form>
     </div>
   );
 }
